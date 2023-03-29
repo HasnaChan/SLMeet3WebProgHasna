80b57c1605c5b295a871449ab96a7a979532ca6c
Hasna Salsabilla <hasnas291@gmail.com>
2023-03-29 21:50:40 +0700

    resume

--- resources/views/resume.blade.php
+++ resources/views/resume.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>About</title>
+    <title>Home</title>
 
     <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Satisfy" rel="stylesheet">
@@ -24,24 +24,13 @@
 </head>
 <body data-bs-spy="scroll" data-bs-target=".navbar" data-bs-offset="50">
 
-  <!-- Background image -->
-    {{-- <div
-      class="bg-image"
-      style="
-        background-image: url('https://mdbcdn.b-cdn.net/img/Photos/Others/images/76.webp');
-        height: 100vh;
-      "
-    ></div> --}}
-  <!-- Background image -->
-
-
   <!-- ======= Header ======= -->
   <header id="header" class="fixed-top d-flex justify-content-center align-items-center">
 
     <nav id="navbar" class="navbar" >
       <ul>
         <li><a class="nav-link scrollto " href="/home">Home</a></li>
-        <li><a class="nav-link scrollto " href="/about">About</a></li>
+        <li><a class="nav-link scrollto" href="/about">About</a></li>
         <li><a class="nav-link scrollto active" href="/resume">Resume</a></li>
         <li><a class="nav-link scrollto " href="/blog">Blog</a></li>
         <li><a class="nav-link scrollto" href="/contact">Contact</a></li>
@@ -49,96 +38,92 @@
     </nav><!-- .navbar -->
   </header><!-- End Header -->
 
-  {{-- KONTEN --}}
-   <!-- ======= My Resume Section ======= -->
-   <section id="resume" class="resume">
-    <div class="container">
 
-      <div class="section-title">
-        <span>My Resume</span>
-        <h2>My Resume</h2>
-        <p>This is my Resume of education.</p>
-      </div>
+    {{-- KONTEN --}}
 
-      <div class="row">
-        <div class="col-lg-6">
-          <h3 class="resume-title">Sumary</h3>
-          <div class="resume-item pb-0">
-            <h4>Alice Barkley</h4>
-            <p><em>Innovative and deadline-driven Graphic Designer with 3+ years of experience designing and developing user-centered digital/print marketing material from initial concept to final, polished deliverable.</em></p>
-            <p>
-            <ul>
-              <li>Portland par 127,Orlando, FL</li>
-              <li>(123) 456-7891</li>
-              <li>alice.barkley@example.com</li>
-            </ul>
-            </p>
+    <!-- ======= My Resume Section ======= -->
+    <section id="resume" class="resume">
+      <div class="container">
+
+        <div class="section-title">
+          <span>My Resume</span>
+          <h2>My Resume</h2>
+          <p>This page will show you my educational background and my experiences.</p>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-6">
+
+            <h3 class="resume-title">Education</h3>
+            <div class="resume-item">
+              <h4>Senior High School</h4>
+              <h5>2017 - 2021</h5>
+              <p><em>SMK Negeri 07 Semarang, Jawa Tengah</em></p>
+              <p>I studied as a vocational sudent for 4 years because my school has 4 years study program for each department.</p>
+            </div>
+            <div class="resume-item">
+              <h4>Junior High School</h4>
+              <h5>2014 - 2017</h5>
+              <p><em>SMP Negeri 02 RSBI, Semarang</em></p>
+              <p>I studied elementary school for 2 years. At this time I was still active as a Taekwondo athlete.</p>
+            </div>
+            <div class="resume-item">
+              <h4>Elementary School</h4>
+              <h5>2009 - 2014</h5>
+              <p><em>SD Negeri Palebon 03 Semarang, Jawa Tengah</em></p>
+              <p>I studied elementary school for 6 years. At this time I was still active as a Taekwondo athlete.</p>
+            </div>
           </div>
-
-          <h3 class="resume-title">Education</h3>
-          <div class="resume-item">
-            <h4>Master of Fine Arts &amp; Graphic Design</h4>
-            <h5>2015 - 2016</h5>
-            <p><em>Rochester Institute of Technology, Rochester, NY</em></p>
-            <p>Qui deserunt veniam. Et sed aliquam labore tempore sed quisquam iusto autem sit. Ea vero voluptatum qui ut dignissimos deleniti nerada porti sand markend</p>
-          </div>
-          <div class="resume-item">
-            <h4>Bachelor of Fine Arts &amp; Graphic Design</h4>
-            <h5>2010 - 2014</h5>
-            <p><em>Rochester Institute of Technology, Rochester, NY</em></p>
-            <p>Quia nobis sequi est occaecati aut. Repudiandae et iusto quae reiciendis et quis Eius vel ratione eius unde vitae rerum voluptates asperiores voluptatem Earum molestiae consequatur neque etlon sader mart dila</p>
+          <div class="col-lg-6">
+            <h3 class="resume-title">Professional Experience</h3>
+            <div class="resume-item">
+              <h4>Senior graphic design specialist</h4>
+              <h5>2019 - Present</h5>
+              <p><em>Experion, New York, NY </em></p>
+              <p>
+              <ul>
+                <li>Lead in the design, development, and implementation of the graphic, layout, and production communication materials</li>
+                <li>Delegate tasks to the 7 members of the design team and provide counsel on all aspects of the project. </li>
+                <li>Supervise the assessment of all graphic materials in order to ensure quality and accuracy of the design</li>
+                <li>Oversee the efficient use of production project budgets ranging from $2,000 - $25,000</li>
+              </ul>
+              </p>
+            </div>
+            <div class="resume-item">
+              <h4>Graphic design specialist</h4>
+              <h5>2017 - 2018</h5>
+              <p><em>Stepping Stone Advertising, New York, NY</em></p>
+              <p>
+              <ul>
+                <li>Developed numerous marketing programs (logos, brochures,infographics, presentations, and advertisements).</li>
+                <li>Managed up to 5 projects or tasks at a given time while under pressure</li>
+                <li>Recommended and consulted with clients on the most appropriate graphic design</li>
+                <li>Created 4+ design presentations and proposals a month for clients and account managers</li>
+              </ul>
+              </p>
+            </div>
           </div>
         </div>
-        <div class="col-lg-6">
-          <h3 class="resume-title">Professional Experience</h3>
-          <div class="resume-item">
-            <h4>Senior graphic design specialist</h4>
-            <h5>2019 - Present</h5>
-            <p><em>Experion, New York, NY </em></p>
-            <p>
-            <ul>
-              <li>Lead in the design, development, and implementation of the graphic, layout, and production communication materials</li>
-              <li>Delegate tasks to the 7 members of the design team and provide counsel on all aspects of the project. </li>
-              <li>Supervise the assessment of all graphic materials in order to ensure quality and accuracy of the design</li>
-              <li>Oversee the efficient use of production project budgets ranging from $2,000 - $25,000</li>
-            </ul>
-            </p>
-          </div>
-          <div class="resume-item">
-            <h4>Graphic design specialist</h4>
-            <h5>2017 - 2018</h5>
-            <p><em>Stepping Stone Advertising, New York, NY</em></p>
-            <p>
-            <ul>
-              <li>Developed numerous marketing programs (logos, brochures,infographics, presentations, and advertisements).</li>
-              <li>Managed up to 5 projects or tasks at a given time while under pressure</li>
-              <li>Recommended and consulted with clients on the most appropriate graphic design</li>
-              <li>Created 4+ design presentations and proposals a month for clients and account managers</li>
-            </ul>
-            </p>
-          </div>
-        </div>
+
       </div>
+    </section><!-- End My Resume Section -->
 
-    </div>
-  </section><!-- End My Resume Section -->
 
+    
   <!-- ======= Footer ======= -->
   <footer id="footer">
     <div class="container">
       <h3>Hasna Salsabilla</h3>
+      <h6>Contact me on:</h6>
       <div class="social-links">
         <a href="https://www.instagram.com/salsabilla.hasna/" class="instagram"><i class="bx bxl-instagram"></i></a>
         <a href="https://www.linkedin.com/in/hasna-salsabilla-abdullah-189914249/s" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+        <a href="https://wa.me/081515856062" class="whatsapp"><i class="bx bxl-whatsapp"></i></a>
       </div>
-      <div class="copyright">
-        &copy; Copyright <strong><span></span></strong>. All Rights Reserved
-      </div>
-      <div class="credits">
-        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-      </div>
+      <div class="">Jl. Pakuan No.3, Sumur Batu, Kec. Babakan Madang, Kabupaten Bogor, Jawa Barat 16810 </div>
     </div>
   </footer><!-- End Footer -->
+
 
 </body>
 <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
