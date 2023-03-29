da0cc4ef87a4f7d4baf59dc8ece5034630eb6203
Hasna Salsabilla <hasnas291@gmail.com>
2023-03-29 22:32:29 +0700

    js

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -146,19 +146,12 @@
   </footer><!-- End Footer -->
 
 </body>
+
+<!-- JS Files -->
 <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-<!-- Vendor JS Files -->
-  <script src="node_modules/purecounter/purecounter_vanilla.js"></script>
-  <script src="node_modules/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="node_modules/glightbox/js/glightbox.min.js"></script>
-  <script src="node_modules/isotope-layout/isotope.pkgd.min.js"></script>
-  <script src="node_modules/swiper/swiper-bundle.min.js"></script>
   <script src="node_modules/waypoints/noframework.waypoints.js"></script>
-  <script src="node_modules/php-email-form/validate.js"></script>
 
 <!-- Template Main JS File -->
 <script src="node_modules/bootstrap/dist/js/main.js"></script>
 
-{{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script> --}}
-  
 </html>
